8ebdeed65364885cc430ab12ed23c407e0f70a78
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-09-09 10:30:16 +0200

    Add a page and AddRazorPages() to the builder

--- MinimalWebAPI/Program.cs
+++ MinimalWebAPI/Program.cs
@@ -1,19 +1,24 @@
 
-// Create a builder with a instance of WebApplication
-using System.Runtime.CompilerServices;
 
+// Create a builder with a instance of WebApplication
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-
+app.UseStaticFiles();
+app.MapRazorPages();
 
 // Routes for the endpoints
-app.MapGet("/", () => "Home page");
-app.MapGet("/about", () => "About page");
-app.MapGet("/contact", () => "Contact page");
-app.MapGet("/products/{category}", (string category) => $"Product is of category: {category}");
-app.MapGet("/products/{category}/items/{id:int}", (string category, int id) => $"Product is of category: {category} with id: {id}");
-app.MapGet("/{random}", (string random) => $"The page {random} does not exist." );
+
+//app.MapGet("/", () => );
+//app.MapGet("/about", () => "About page");
+//app.MapGet("/contact", () => "Contact page");
+//app.MapGet("/products/{category}", (string category) => $"Product is of category: {category}");
+//app.MapGet("/products/{category}/items/{id:int}", (string category, int id) => $"Product is of category: {category} with id: {id}");
+//app.MapGet("/{random}", (string random) => $"The page {random} does not exist." );
+
+app.MapGet("/", static () => Results.Redirect("/Index"));
 
 // Run the application
 app.Run();
