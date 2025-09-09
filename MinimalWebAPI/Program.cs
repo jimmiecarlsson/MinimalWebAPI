9bf655a4edf53a95f7b2a68e9d73461074b8d226
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-09-09 09:54:00 +0200

    Add 404 route

--- MinimalWebAPI/Program.cs
+++ MinimalWebAPI/Program.cs
@@ -1,5 +1,7 @@
 
 // WebApplication example for .NET 9.0
+using System.Runtime.CompilerServices;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -10,7 +12,8 @@ app.MapGet("/", () => "Home page");
 app.MapGet("/about", () => "About page");
 app.MapGet("/contact", () => "Contact page");
 app.MapGet("/products/{category}", (string category) => $"Product is of category: {category}");
-
+app.MapGet("/products/{category}/items/{id:int}", (string category, int id) => $"Product is of category: {category} with id: {id}");
+app.MapGet("/{random}", (string random) => $"The page {random} does not exist." );
 
 // Run the application
 app.Run();
