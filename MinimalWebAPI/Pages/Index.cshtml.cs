4be3d8d724d30bbc80f790bdd750ef2ddc438a2c
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-09-09 11:00:20 +0200

    Add UseStaticFiles() and the IndexModel

--- MinimalWebAPI/Pages/Index.cshtml.cs
+++ MinimalWebAPI/Pages/Index.cshtml.cs
@@ -1,12 +1,17 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MinimalWebAPI.Pages
 {
     public class IndexModel : PageModel
     {
+        // Property som kan användas i cshtml
+        public string Meddelande { get; private set; } = "Tjena ifrån PageModel-klassen!";
+    
+    // Körs när sidan laddas med GET
         public void OnGet()
         {
+            Meddelande = $"Sidan laddades {DateTime.Now}";
         }
+
     }
 }
